eb72aa4360c1ec776a1f0d5fed13dc3514db4166
Mateusz Sip <mateusz.sip@gmail.com>
2017-01-28 17:37:09 +0100

    Updated configs

---
+++ .config/fish/conf.d/omf.fish
@@ -0,0 +1,13 @@
+#export TERM='xterm-256color'
+
+set -g theme_color_scheme base16
+
+#Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+set -U FZF_LEGACY_KEYBINDINGS 0
+
+# Load Oh My Fish configuration.
+source $OMF_PATH/init.fish
